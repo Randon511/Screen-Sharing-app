249ae8550e1d8cca91771a690d4c4e4ed3ed5f5e
Randon <randon551@gmail.com>
2020-06-23 13:58:39 -0600

    Worked out peer-peer connections, added video controls, added a stop streaming button, added a counter to show number of users

--- server.js
+++ server.js
@@ -2,19 +2,48 @@ const io = require('socket.io')(8000)
 const users = {}
 
 io.on('connection', socket => {
-	//When a new user joins, save their name and notify other users
-	socket.on('new-user', name =>
+	//When a new user joins:
+	//Send them a list of all other users in the room
+	//Send every other user the information of the new user
+	socket.on('new-user', payload =>
 	{
-		users[socket.id] = name
-		console.log("conn")
+		users[socket.id] = {socketId: socket.id, name: payload.name}
+		const list = []
+		const entries = Object.entries(users)
+		entries.forEach(user => {
+			if(socket.id != user[1].socketId){
+				list.push(user)
+			}
+		})
+
+		if(list.length != 0)
+		{
+			socket.emit("all-users", list)
+			socket.broadcast.emit("user-joined-room", users[socket.id])
+		}
 	})
 
-	//When a new message is recieasdved, sent it to all users
-	socket.on('send-chat-message', data => {
+	socket.on('sending-signal', payload =>
+	{
+		io.to(payload.userToSignal).emit('recieving-signal', {signal: payload.signal, callerId: payload.callerId})
+	})
+
+	socket.on('returning-signal', payload =>
+	{
+		io.to(payload.callerId).emit('recieving-returned-signal', {signal: payload.signal, id: socket.id})
+	})
+
+	socket.on('disconnect', () =>
+	{
+		delete users[socket.id]
+		socket.broadcast.emit('user-left-room', socket.id)
+	})
+	
+	//When a new message recieved, sent it to all users
+	socket.on('send-chat-message', message => {
 		//Broadcasts the chat message as well as the users name to all other sockets
-		socket.emit('chat-message', {message: data, name: users[socket.id]})
-		socket.broadcast.emit('chat-message', {message: data, name: users[socket.id]})
+		socket.emit('chat-message', {message: message, name: users[socket.id].name})
+		socket.broadcast.emit('chat-message', {message: message, name: users[socket.id].name})
 	})
 })
-
 
